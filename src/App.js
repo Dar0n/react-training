63d89818b8a2128bfecdeff2883a9511a20ff44d
Ilya <shatuncheg@gmail.com>
2018-09-17 12:43:14 +0200

    Add propTypes npm package. Add txt prop and default nmbr prop.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-const App = () => <h1>Hello</h1>
+class App extends React.Component {
+  render() {
+    let txt = this.props.txt;
+    return (
+      <h1>{txt}</h1>
+    )
+  }
+}
+
+// const App = () => <h1>Olala!</h1>
+App.propTypes = {
+  txt: propTypes.string,
+  nmbr: propTypes.number.isRequired,
+}
+
+App.defaultProps = {
+  nmbr: 5,
+}
 
 export default App;
