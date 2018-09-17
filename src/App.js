0598433b13a742d5f938c81f21a708c1a0fba00a
Ilya <shatuncheg@gmail.com>
2018-09-17 12:52:15 +0200

    Try state of the components and setState method. Update input field from the component state while typing.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'Some random text',
+    }
+
+  }
+  update (e) {
+    this.setState({
+      txt: e.target.value,
+    })
+  }
   render() {
     let txt = this.props.txt;
     return (
-      <h1>{txt}</h1>
+      <div>
+        <input type='text' onChange={this.update.bind(this)}></input>
+        <h1>{this.state.txt}</h1>
+      </div>
     )
   }
 }
